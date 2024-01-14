6a6e8751663a3394736ec6c3acd4b4a36fb3815e
777gt <santillansat@gmail.com>
2024-01-14 15:00:20 -0600

    Add application file

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import gradio as gr
 
 def greet(name):
     return "Hello " + name + "!!"
+  
+with gr.Blocks(title="🔊",theme=gr.themes.Base(primary_hue="rose",neutral_hue="zinc")) as app:
+    with gr.Row():
+        gr.HTML("<img  src='file/a.png' alt='image'>")
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+app.launch()
